828351adbcfa5c1717fc24a1db27fe207718e4ab
ManuelCAZALLA <manuelcm23@hotmail.com>
2025-06-22 11:30:13 +0200

    Creo el repo

---
+++ UbiCar/UbiCarApp.swift
@@ -0,0 +1,17 @@
+//
+//  UbiCarApp.swift
+//  UbiCar
+//
+//  Created by Manuel Cazalla Colmenero on 22/6/25.
+//
+
+import SwiftUI
+
+@main
+struct UbiCarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ UbiCarTests/UbiCarTests.swift
@@ -0,0 +1,17 @@
+//
+//  UbiCarTests.swift
+//  UbiCarTests
+//
+//  Created by Manuel Cazalla Colmenero on 22/6/25.
+//
+
+import Testing
+@testable import UbiCar
+
+struct UbiCarTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
