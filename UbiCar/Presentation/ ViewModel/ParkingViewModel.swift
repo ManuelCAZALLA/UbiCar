6223946f68ec4b7e1579b4841d345a33a07cee29
ManuelCAZALLA <manuelcm23@hotmail.com>
2025-06-29 11:12:30 +0200

    [Modularizacion y Localizable] Separo vistas masivas en subvistas y creo Localizable

--- UbiCar/Presentation/ ViewModel/ParkingViewModel.swift
+++ UbiCar/Presentation/ ViewModel/ParkingViewModel.swift
@@ -1,17 +1,36 @@
 import Foundation
 import CoreLocation
 import Combine
+import AVFoundation
 
-class ParkingViewModel: ObservableObject {
+final class ParkingViewModel: NSObject, ObservableObject {
     @Published var lastParking: ParkingLocation?
+    @Published var userLocation: CLLocationCoordinate2D?
+    @Published var authorizationStatus: CLAuthorizationStatus = .notDetermined
+    @Published var placeName: String?
+    @Published var showingAlert = false
     
     private let parkingKey = "lastParkingLocation"
+    private let locationManager = CLLocationManager()
+    private let speechSynthesizer = AVSpeechSynthesizer()
     
-    init() {
+    override init() {
+        super.init()
+        locationManager.delegate = self
+        authorizationStatus = locationManager.authorizationStatus
         loadParkingLocation()
+        
+        if authorizationStatus == .authorizedWhenInUse || authorizationStatus == .authorizedAlways {
+            locationManager.startUpdatingLocation()
+        }
     }
     
-    func saveParkingLocation(coordinate: CLLocationCoordinate2D, placeName: String?) {
+    func requestLocation() {
+        locationManager.requestWhenInUseAuthorization()
+    }
+    
+    func saveParkingLocation() {
+        guard let coordinate = userLocation else { return }
         let parking = ParkingLocation(
             latitude: coordinate.latitude,
             longitude: coordinate.longitude,
@@ -21,6 +40,7 @@ class ParkingViewModel: ObservableObject {
         if let data = try? JSONEncoder().encode(parking) {
             UserDefaults.standard.set(data, forKey: parkingKey)
             lastParking = parking
+            showingAlert = true
         }
     }
     
@@ -35,4 +55,54 @@ class ParkingViewModel: ObservableObject {
         UserDefaults.standard.removeObject(forKey: parkingKey)
         lastParking = nil
     }
-} 
+    
+    // MARK: - Nueva función para actualizar el nombre del lugar
+    func updatePlaceName() {
+        guard let location = userLocation else { return }
+        let loc = CLLocation(latitude: location.latitude, longitude: location.longitude)
+        CLGeocoder().reverseGeocodeLocation(loc) { [weak self] placemarks, error in
+            if let placemark = placemarks?.first {
+                let name = placemark.name ?? placemark.locality ?? placemark.country
+                DispatchQueue.main.async {
+                    self?.placeName = name
+                }
+            } else {
+                DispatchQueue.main.async {
+                    self?.placeName = nil
+                }
+            }
+        }
+    }
+    
+    // MARK: - Nueva función para hablar la distancia al coche
+    func speakDistance(to parking: ParkingLocation) {
+        guard let userLoc = userLocation else { return }
+        let userLocation = CLLocation(latitude: userLoc.latitude, longitude: userLoc.longitude)
+        let carLocation = CLLocation(latitude: parking.latitude, longitude: parking.longitude)
+        let distance = userLocation.distance(from: carLocation)
+        let texto = "Tu coche está a \(Int(distance)) metros."
+        let utterance = AVSpeechUtterance(string: texto)
+        utterance.voice = AVSpeechSynthesisVoice(language: "es-ES")
+        speechSynthesizer.speak(utterance)
+    }
+}
+
+extension ParkingViewModel: CLLocationManagerDelegate {
+    func locationManagerDidChangeAuthorization(_ manager: CLLocationManager) {
+        authorizationStatus = manager.authorizationStatus
+        if authorizationStatus == .authorizedWhenInUse || authorizationStatus == .authorizedAlways {
+            locationManager.startUpdatingLocation()
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        guard let newLocation = locations.last else { return }
+        userLocation = newLocation.coordinate
+        updatePlaceName()  // Llamamos aquí para mantener placeName actualizado
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        userLocation = nil
+        placeName = nil
+    }
+}
---
+++ UbiCar/Resources/String+Localization.swift
@@ -0,0 +1,26 @@
+//
+//  String+Localization.swift
+//  UbiCar
+//
+//  Created by Manuel Cazalla Colmenero on 22/6/25.
+//
+
+import Foundation
+
+extension String {
+    
+    /// Returns a localized version of the string
+    var localized: String {
+        return NSLocalizedString(self, comment: "")
+    }
+    
+    /// Returns a localized version of the string with format arguments
+    func localized(with arguments: CVarArg...) -> String {
+        return String(format: self.localized, arguments: arguments)
+    }
+    
+    /// Returns a localized version of the string with format arguments
+    func localized(with arguments: [CVarArg]) -> String {
+        return String(format: self.localized, arguments: arguments)
+    }
+} 
